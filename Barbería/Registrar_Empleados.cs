49ca4b0bb138a41e3e04bbbfd8d67668a7b4c571
Divanny <67510588+Divanny@users.noreply.github.com>
2022-07-06 16:59:06 -0400

    Add files via upload

---
+++ Barbería/Registrar_Empleados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Barberia
+{
+    public partial class Registrar_Empleados : Form
+    {
+        public Registrar_Empleados()
+        {
+            InitializeComponent();
+        }
+    }
+}
